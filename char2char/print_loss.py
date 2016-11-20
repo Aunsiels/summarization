93679481c4494853cebb054bad71f517a602ed16
Julien Romero <romerojulien34@gmail.com>
2016-11-20 16:58:10 +0100

    print loss modify

--- char2char/print_loss.py
+++ char2char/print_loss.py
@@ -8,7 +8,8 @@ rougel = []
 rouges = []
 loss = []
 i = 0
-N = 50
+N = 10
+BATCHSIZE = 64
 
 for j in f:
     if i%6 == 0:
@@ -25,12 +26,25 @@ for j in f:
         loss += list(map(float, j.split(',')))
     i += 1
 
+rouge1 = list(map(lambda x: x / BATCHSIZE, rouge1))
+rouge2 = list(map(lambda x: x / BATCHSIZE, rouge2))
+rouge3 = list(map(lambda x: x / BATCHSIZE, rouge3))
+rougel = list(map(lambda x: x / BATCHSIZE, rougel))
+rouges = list(map(lambda x: x / BATCHSIZE, rouges))
+
 rouge1 = np.convolve(rouge1, np.ones((N,))/N, mode='valid')
 rouge2 = np.convolve(rouge2, np.ones((N,))/N, mode='valid')
 rouge3 = np.convolve(rouge3, np.ones((N,))/N, mode='valid')
 rougel = np.convolve(rougel, np.ones((N,))/N, mode='valid')
 rouges = np.convolve(rouges, np.ones((N,))/N, mode='valid')
 loss = np.convolve(loss, np.ones((N,))/N, mode='valid')
+
+print("rouge1 last :" + str(rouge1[-1] * 100))
+print("rouge2 last :" + str(rouge2[-1] * 100))
+print("rouge3 last :" + str(rouge3[-1] * 100))
+print("rougel last :" + str(rougel[-1] * 100))
+print("rouges last :" + str(rouges[-1] * 100))
+
 plt.plot(rouge1, label="rouge1")
 plt.plot(rouge2, label="rouge2")
 plt.plot(rouge3, label="rouge3")
